b94d065b61d3a2b2fc3c309c571eabb5774a0473
gnahc2305 <achangmurillo@gmail.com>
2022-11-22 18:57:04 -0500

    add thumbnails

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Card({ item }) {
+function Card({ item: { id: {videoId}, snippet} }) {
     // console.log(item.snippet.title)
   return (
     <div className='bg-slate-700 rounded-md md:h-[250px] md:w-[250px] h-[200px] w-[200px]'>
-        <div className='bg-red-900 h-[55%] rounded-md'>
-            img
+        <div className='h-[65%] rounded-md'>
+            <img src={snippet?.thumbnails?.high?.url} alt="thumbnail" className='h-[100%] w-[100%]' />
         </div>
 
         <div className=''>
-            <h1 className='text-white p-2 text-md md:text-green'>{item.snippet.title}</h1>
+            <h1 className='text-white p-2 text-sm'>{snippet.title}</h1>
         </div>
     </div>
   )
