233f96265bcfaadd1406bfac3982ca98c09b8041
gnahc2305 <achangmurillo@gmail.com>
2022-11-25 21:49:18 -0500

    videos are now clickeable

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
+let ytLink = 'https://www.youtube.com/watch?v='
+
 function Card({ item: { id: {videoId}, snippet} }) {
-    // console.log(item.snippet.title)
   return (
-    <div className='rounded-md md:h-[250px] md:w-[250px] h-[200px] w-[200px]'>
+    <div className='rounded-md md:h-[250px] md:w-[250px] h-[200px] w-[200px] cursor-pointer'>
+      <a href={ytLink + videoId}>
         <div className='h-[65%] rounded-md'>
             <img src={snippet?.thumbnails?.high?.url} alt="thumbnail" className='h-[100%] w-[100%]' />
         </div>
-
         <div className=''>
             <h1 className='text-black p-2 text-sm'>{snippet.title}</h1>
+            <h5>{snippet.channelTitle}</h5>
         </div>
+      </a>
     </div>
   )
 }
