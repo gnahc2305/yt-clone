548709234bb90e918f9ccdd1b5bb3724ea68c78f
gnahc2305 <achangmurillo@gmail.com>
2022-11-27 15:28:37 -0500

    channel image doesnt work

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+let channelLink = 'https://www.youtube.com/@';
+
 function ChannelCard({ item }) {
-  console.log(item.snippet?.thumbnails?.high?.url);
+  console.log(item.snippet?.thumbnails);
+  // console.log(item);
   return (
-    <div className='bg-gray-800 md:h-[200px] md:w-[200px] rounded-full h-[150px] w-[150px] m-2'>
-      <img src={item.snippet?.thumbnails?.high?.url} alt="profile" srcset="" />
-    </div>
+    <a href={channelLink + item.snippet.channelTitle} target="_blank" rel="noopener noreferrer">
+      <div className='bg-gray-800 md:h-[200px] md:w-[200px] rounded-full h-[150px] w-[150px] m-2'>
+        <img src={item.snippet?.thumbnails?.default?.url} alt="profile" />
+      </div>
+    </a>
   )
 }
 
