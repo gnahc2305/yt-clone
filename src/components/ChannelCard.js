e9bf16818353d615ab53c21b07615cade2df0f30
gnahc2305 <achangmurillo@gmail.com>
2022-11-27 21:06:46 -0500

    move profile card

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 let channelLink = 'https://www.youtube.com/@';
+const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
+
 
 function ChannelCard({ item }) {
-  console.log(item.snippet?.thumbnails);
+  console.log(item?.snippet?.thumbnails?.high?.url);
   // console.log(item);
   return (
     <a href={channelLink + item.snippet.channelTitle} target="_blank" rel="noopener noreferrer">
-      <div className='bg-gray-800 md:h-[200px] md:w-[200px] rounded-full h-[150px] w-[150px] m-2'>
-        <img src={item.snippet?.thumbnails?.default?.url} alt="profile" />
+      <div className='bg-gray-800 md:h-[200px] md:w-[200px] rounded-full h-[150px] w-[150px] mt-[-40px]'>
+        <img src={item?.snippet?.thumbnails?.high?.url} alt="profile" />
       </div>
     </a>
   )
